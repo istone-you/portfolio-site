88a1682e266f6bd87447ed24851aea2ea0b65f63
istone-you <istoneyou1205@yahoo.co.jp>
2023-08-11 15:51:22 +0900

    ✨ ページのレイアウト

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="p-5 pb-12 w-90 m-auto border-2 border-black my-10 mx-24 card-shadow">
+      <Component {...pageProps} />
+    </div>
+  );
 }

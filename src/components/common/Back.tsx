6fd1739d26e6a85da6d8b6519c9802d041c6bcfe
istone-you <istoneyou1205@yahoo.co.jp>
2023-08-11 22:15:26 +0900

    戻るボタン追加

---
+++ src/components/common/Back.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+const Back = () => {
+  const router = useRouter();
+  return (
+    <div className="mt-10 flex items-center justify-center mb-10">
+      <Link
+        href="#"
+        onClick={() => {
+          router.back();
+        }}
+      >
+        <Image src="/image/home.png" alt="" width={50} height={50} />
+      </Link>
+    </div>
+  );
+};
+
+export default Back;

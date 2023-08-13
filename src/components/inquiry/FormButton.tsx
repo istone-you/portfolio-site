ad9e6b5ef28bc85f0ccbf52e78dc7574008ef80f
istone-you <istoneyou1205@yahoo.co.jp>
2023-08-13 17:17:37 +0900

    💄 微修正

--- src/components/inquiry/FormButton.tsx
+++ src/components/inquiry/FormButton.tsx
@@ -5,7 +5,7 @@ const FormButton = (props: FormButtonProps) => {
     <div className="mt-6 flex items-center justify-center">
       <button
         type={props.type}
-        className="bg-gray-300 px-4 py-1 font-bold border border-black button-shadow hover:shadow-none hover:translate-y-1 hover:translate-x-1"
+        className="text-black bg-gray-300 px-4 py-1 font-bold border border-black button-shadow hover:shadow-none hover:translate-y-1 hover:translate-x-1"
       >
         {props.label}
       </button>

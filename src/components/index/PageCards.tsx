8284c8549c9481f6955f925f93dc56d9cdd1747f
istone-you <istoneyou1205@yahoo.co.jp>
2023-08-14 16:39:03 +0900

    💫 fade inアニメーションの変更

--- src/components/index/PageCards.tsx
+++ src/components/index/PageCards.tsx
@@ -6,7 +6,7 @@ const PageCards = ({ pages }: { pages: Pages }) => {
   return (
     <div className="mx-auto max-w-3xl center-wrap">
       {pages.map((page: Page) => (
-        <div className="fade-in-third px-12" key={page.id}>
+        <div className="fade-in-second px-12" key={page.id}>
           <Link href={page.path} className="no-underline">
             <div className="mb-1 mt-10 mx-auto py-4 px-10 h-32 w-72 index-card">
               <p>
--- src/components/index/Title.tsx
+++ src/components/index/Title.tsx
@@ -4,23 +4,21 @@ import type { Index } from "@/types/index";
 
 const Title = ({ index }: { index: Index }) => {
   return (
-    <>
-      <div className="fade-in-first">
-        <div className="center">
-          <Image
-            src={index.thumbnail.url}
-            alt="thumbnail"
-            width={72}
-            height={72}
-            className="mt-12 mb-6 rounded-full border border-black"
-          />
-        </div>
-        <h1 className="sm:text-2xl mb-12 center">{index.greeting}</h1>
+    <div className="fade-in-first">
+      <div className="center">
+        <Image
+          src={index.thumbnail.url}
+          alt="thumbnail"
+          width={72}
+          height={72}
+          className="mt-12 mb-6 rounded-full border border-black"
+        />
       </div>
-      <p className="fade-in-second mx-12 md:text-xl font-normal center">
+      <h1 className="sm:text-2xl mb-12 center">{index.greeting}</h1>
+      <p className="mx-12 md:text-xl font-normal center">
         <b>{index.thanks}</b>
       </p>
-    </>
+    </div>
   );
 };
 

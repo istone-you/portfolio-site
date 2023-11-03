565126bea7b85f54c984d08d1503b696616a5f22
istone-you <istoneyou1205@yahoo.co.jp>
2023-11-03 11:26:45 +0900

    💄 aboutページのレイアウト変更

--- src/components/about/Profile.tsx
+++ src/components/about/Profile.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
 import type { Index } from "@/types/index";
 
 const Profile = ({ index }: { index: Index }) => {
   return (
-    <div className="fade-in-second py-16 px-8 sm:px-20">
-      <h1 className="text-2xl mb-10 center">石井 湧</h1>
+    <div className="fade-in-second py-6 px-8 sm:px-20">
+      <div className="center">
+        <Image
+          src={index.thumbnail.url}
+          alt="thumbnail"
+          width={100}
+          height={100}
+          className="my-4 rounded-full border border-black"
+        />
+      </div>
+      <h1 className="text-3xl mb-10 center">石井 湧</h1>
       <div
         className="mb-4 prose"
         dangerouslySetInnerHTML={{

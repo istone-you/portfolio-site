8043495845f0824fd56ab9ac96ac157fd76cfd89
istone-you <istoneyou1205@yahoo.co.jp>
2024-06-27 17:58:34 +0900

    chore: Update npm dependencies, including @vercel/analytics to version 1.3.1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 import "@/styles/globals.css";
 import "@/styles/fade-in.css";
@@ -28,6 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
